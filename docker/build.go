d316dbf25878d64f5fae3e2d58198f420ef385f8
Julien Kassar <dev@julienk.me>
2026-08-09 22:58:27 +0200

    refactor(docker): move Build function to dedicated build.go file

---
+++ docker/build.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"strings"
+
+	"dagger/docker/internal/dagger"
+)
+
+func (m *Docker) Build(
+	ctx context.Context,
+	source *dagger.Directory,
+	platform dagger.Platform,
+	// +optional
+	// +default="Dockerfile"
+	dockerfile string,
+	// +optional
+	buildArgs []string,
+	// +optional
+	secrets []*dagger.Secret,
+) *dagger.Container {
+	opts := dagger.ContainerBuildOpts{
+		Dockerfile: dockerfile,
+	}
+	if len(buildArgs) > 0 {
+		var args []dagger.BuildArg
+
+		for _, arg := range buildArgs {
+			nv := strings.Split(arg, "=")
+
+			args = append(args, dagger.BuildArg{Name: nv[0], Value: nv[1]})
+		}
+		opts.BuildArgs = args
+	}
+	if len(secrets) > 0 {
+		opts.Secrets = secrets
+	}
+
+	ctr := dag.Container(dagger.ContainerOpts{Platform: platform}).
+		Build(source, opts)
+
+	return ctr
+}
